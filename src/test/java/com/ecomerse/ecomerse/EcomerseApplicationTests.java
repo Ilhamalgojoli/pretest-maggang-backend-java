071d6f5e4bfe5afcdbb96016625472f0e100ad1f
Ilham Al Gojali <rilhamgojali123@gmail.com>
2025-07-31 18:44:31 +0700

    Setup project ecommers

---
+++ src/test/java/com/ecomerse/ecomerse/EcomerseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomerse.ecomerse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomerseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
